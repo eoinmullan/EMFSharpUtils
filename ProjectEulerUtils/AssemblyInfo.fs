eba48541683851438916640680fdbf60e7a00157
eoinmullan <eoinmullan@hotmail.com>
2015-08-22 09:22:03 +0100

    Adding appveyor configuration file for a publish to Nuget project, and adding nuspec file for creating nuget package.

--- ProjectEulerUtils/AssemblyInfo.fs
+++ ProjectEulerUtils/AssemblyInfo.fs
@@ -4,6 +4,7 @@ open System.Reflection
 
 [<assembly: AssemblyTitle("ProjectEulerUtils")>]
 [<assembly: AssemblyDescription("A collection of functionality that helps solve the problems on Project Euler.")>]
+[<assembly: AssemblyCompany("Eoin Mullan")>]
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
