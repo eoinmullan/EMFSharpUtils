e9105e33470601a2329322369c7675e7cfc2f832
eoinmullan <eoinmullan@hotmail.com>
2015-09-13 22:41:37 +0100

    Adding fibonacci sequency function and unit tests.

--- ProjectEulerUtils/Library1.fs
+++ ProjectEulerUtils/Library1.fs
@@ -1,9 +1,21 @@
 ﻿namespace ProjectEuler
 
-module Utils =
+open System.Collections.Generic;
 
-    let rec nthFibonacci x =
+module PEUtils =
+
+    let fibSeq =
+        let rec fibSeq n1 n2 =
+            seq { let n0 = n1 + n2 
+                  yield n0
+                  yield! fibSeq n0 n1 }
+        seq { yield 1 ; yield 1 ; yield! (fibSeq 1 1) }
+
+    let firstNFibs x =
+        fibSeq |> Seq.take x
+
+    let rec nthFib x =
         match x with
-        | 0 -> 0
+        | 0 -> 1
         | 1 -> 1
-        | _ -> x + nthFibonacci (x-1)
+        | _ -> nthFib (x-1) + nthFib (x-2)
---
+++ ProjectEulerUtilsTest/AcceptanceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace ProjectEulerUtilsTest {
+    [TestClass]
+    public class AcceptanceTests {
+        [TestMethod]
+        public void ShouldGenerateFibbonacciSequence() {
+            CollectionAssert.AreEqual(new List<int>() { 1, 1, 2, 3, 5 }, PEUtils.fibSeq.Take(5).ToList());
+            CollectionAssert.AreEqual(new List<int>() { 1, 1, 2, 3, 5 }, PEUtils.firstNFibs(5).ToList());
+            CollectionAssert.AreEqual(First20ElementsOfFibonacciSequence(), PEUtils.fibSeq.Take(20).ToList());
+            CollectionAssert.AreEqual(First20ElementsOfFibonacciSequence(), PEUtils.firstNFibs(20).ToList());
+        }
+
+        [TestMethod]
+        public void ShouldGenerateNthFibonacciNumber() {
+            for (var i = 0; i < 20; i++) {
+                Assert.AreEqual(First20ElementsOfFibonacciSequence()[i], PEUtils.nthFib(i));
+            }
+        }
+
+        private List<int> First20ElementsOfFibonacciSequence() {
+            return new List<int> { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765 };
+        }
+    }
+}
